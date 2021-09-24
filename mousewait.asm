611dfd3b58797ad11b4e568b73047bbd79da7df6
Matthew Deaves <mndeaves@me.com>
2021-09-25 00:13:50 +0100

    added comments

--- mousewait.asm
+++ mousewait.asm
@@ -1,4 +1,17 @@
+; Workflow: The binary loads and wait for the user to click on the LEFT mouse button
+
+; TIPS:
+; On line 15 NO SPACE after the comma or you will get an error by vasm
+; Each line, besides the label have at least one space, that will help Sublime to applying the color scheme that improves readability
+
+; Loop is a label, kind of a go to in the old basic
 loop:
+
+; test if address has the value 6.
  btst #6,$bfe001
+
+; if not, then sux to be you program. Go back to loop and keep repeating this logic
  bne loop
-rts
+
+; condition met (mouse button pressed) exit (return to subroutine)
+ rts
